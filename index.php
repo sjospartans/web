994d978aefed19b3927ce85a18b04d6c73f8d3b1
Nate34 <nate599922@gmail.com>
2016-08-11 10:16:23 -0500

    Lots of files changed etc....

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome :: Nathan Walden Social Links</title>
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/site.css">
+	<link rel="stylesheet" type="text/css" href="css/hover.css">
+	<script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+</head>
+<body background="https://hd.unsplash.com/reserve/mW95rWmYSRe4nTMVaRaW_French-Mountain.jpg">
+
+
+	<div class="container fadeInDown animated">
+		<div class="welcome">
+			<h1>Welcome</h1>
+		</div>
+	</div>
+		<div class="container animated">
+				<center>
+					<li class="slideInLeft animated"><a href="http://www.twitter.com/Nate_w34" target="_blank"><img src="images/twitter.png"></a></li>
+					<li class="fadeInDown animated"><a href="http://www.instagram.com/Nate_w34" target="_blank"><img src="images/instagram.png"></a></li>
+					<li class="slideInRight animated"><a href="https://plus.google.com/u/0/106751646279176189713/posts" target="_blank"><img src="images/google.png"></a></li>
+				</center>
+		</div>
+	
+		<footer>
+			<center>
+				<small>Background Image by <a href="https://unsplash.com/@romainbriaux">Romain Briaux</a> courtesy <a href="https://unsplash.com">Unsplash</small>
+			</center>
+		</footer>
+
+		<p>
+		</p>
+		<center><a href="/css/animate/home">Proceed to Site</a></center>
+
+</body>
+</html>
