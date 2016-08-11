da0039d9bf316725ac9756b6a0d5d40e86c72a8c
Nate34 <nate599922@gmail.com>
2016-08-11 14:52:15 -0500

    added many files

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="css/animate.css">
 	<link rel="stylesheet" type="text/css" href="css/site.css">
 	<link rel="stylesheet" type="text/css" href="css/hover.css">
+  	<!-- Optional theme -->
 	<script src="js/wow.min.js"></script>
     <script>
         new WOW().init();
@@ -24,17 +25,16 @@
 	</div>
 		<div class="container animated">
 				<center>
-					
+					<li class="slideInLeft animated"><a href='/git/web/home'><img src="images/right_arrow.png" alt="Enter Site" value="Enter Site"></a></li>
 				</center>
 		</div>
 	
 		<footer>
 			<center>
-				<small>Background Image by <a href="https://unsplash.com/@romainbriaux">Romain Briaux</a> courtesy <a href="https://unsplash.com">Unsplash</small>
+				<li class="fadeInUp animated"><small>Background Image by <a href="https://unsplash.com/@romainbriaux">Romain Briaux</a> courtesy <a href="https://unsplash.com">Unsplash</small>
 			</center>
 		</footer>
 
-		
 		
 
 </body>
